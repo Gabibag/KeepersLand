b0648640fd4eb093acc957bfedd81ceb921a33f9
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-02-03 11:23:49 -0800

    Modified healing + boss finish up

--- NameHere/Enemies/Bosses/FinalBoss.java
+++ NameHere/Enemies/Bosses/FinalBoss.java
@@ -2,19 +2,29 @@ package NameHere.Enemies.Bosses;
 
 import NameHere.Abstracts.Boss;
 import NameHere.Abstracts.Enemy;
+import NameHere.Main;
 import NameHere.Player;
 
 import java.util.List;
 
 public class FinalBoss extends Boss {
+    public int getBossStage() {
+        return bossStage;
+    }
+
+    public void setBossStage(int bossStage) {
+        this.bossStage = bossStage;
+    }
+
+    private int bossStage = 1;
     public void setBaseStats() {
         this.baseHp = 7500;
-        this.damage = 500;
+        this.damage = 1000;
         this.xp = 12000;
         this.name = "Keeper";
         this.coins = 5000;
         this.tokens = 100;
-
+        bossStage = 1;
     }
 
     @Override
@@ -36,5 +46,35 @@ public class FinalBoss extends Boss {
     @Override
     public void bossOnSpawn(List<Enemy> allies) {
 
+    }
+
+    @Override
+    public int Attack(Player p, List<Enemy> allies) {
+        //1 in 10 chance to make all items that contian the name shard in the player's inventory deal 10 damage to the player
+        int counter = 0;
+        int rand = Main.r.nextInt(10);
+        if (rand == 1) {
+            for (int i = 0; i < p.getInventory().size(); i++) {
+                if (p.getInventory().get(i).getName().contains("Shard")) {
+                    counter++;
+                }
+            }
+            System.out.println("The Keeper's eyes glow with a bright light. You feel a sharp pain in your chest.");
+            return counter*10;
+        }
+        else if (rand==2) {
+            System.out.println(name + " deals " + damage + " damage (DOUBLE)");
+            return damage*2;
+        } else if(rand == 3){
+            for (int i = 0; i < p.getInventory().size(); i++) {
+                if (p.getInventory().get(i).getName().equalsIgnoreCase("Omega Shard")) {
+                    counter++;
+                }
+            }
+            System.out.println("The shards power the Keeper. It deals " + damage * (counter/10) + " damage.");
+            return damage * (counter/10);
+        }
+
+        return damage;
     }
 }
