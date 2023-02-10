98f99a221df39bbad016e25bf77eb3885d45526e
Hibub-Idk <ns9316@pleasantonusd.net>
2023-02-09 21:31:06 -0800

    Works with .jar now

--- NameHere/Main.java
+++ NameHere/Main.java
@@ -1,6 +1,7 @@
 package NameHere;
 
 import NameHere.Abstracts.*;
+import NameHere.Enemies.Lava.Demon;
 import NameHere.Enviroments.AbandonedCity;
 import NameHere.Enviroments.LavaZone;
 import NameHere.Enviroments.NullZone;
@@ -8,9 +9,16 @@ import NameHere.Interacts.Battle;
 import NameHere.Interacts.LevelUp;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 
 
 public class Main {
@@ -33,7 +41,6 @@ public class Main {
         System.out.println(Colors.CLEAR + "Press ctrl + c to quit ;)");
         //defaults for player
         int saves = Helper.getInput("[0] New save \n[1] Load Save", 0, 1);
-
         if (saves == 1) {
             try {
                 player = loadSave();
@@ -228,22 +235,57 @@ public class Main {
     }
 
     public static void getNewPlace() {
-        try {
-            currentPlace = allPlaces.get(r.nextInt(allPlaces.size())).getClass().getDeclaredConstructor().newInstance();
-        
-        while (!currentPlace.isVaild(player)) {
-            currentPlace = allPlaces.get(r.nextInt(allPlaces.size()));
-        }
-    }catch (Exception e){
-        System.out.println("Error getting new place, invaild constructor, trying again");
-        getNewPlace();
-    }
+            try {
+                currentPlace = allPlaces.get(r.nextInt(allPlaces.size())).getClass().getDeclaredConstructor().newInstance();
+            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
     }
 
     /**
      * peforms black magic to get all the types
      **/
     public static void initTypes() {
+        try {
+        for (String classpathEntry : System.getProperty("java.class.path").split(System.getProperty("path.separator"))) {
+            System.out.println("Entry " + classpathEntry);
+           // System.out.println("first loop");
+            if (classpathEntry.endsWith(".jar")) {
+                System.out.println(".jar found");
+                File jar = new File(classpathEntry);
+                JarInputStream is;
+                 is = new JarInputStream(new FileInputStream(jar));
+                JarEntry entry;
+                while( (entry = is.getNextJarEntry()) != null) {
+                    //System.out.println("name" + entry.getName());
+                    if(entry.getName().endsWith(".class")) {
+                      //  System.out.println(entry.getName());
+                        String className = entry.getName().replace(".class", "");
+                     //   className = className.substring(className.indexOf("/")+1);
+                        System.out.println(className);
+                        try {
+                            Class.forName(className.replace("/",".")).newInstance();
+                        } catch (Exception e) {
+                        //    System.out.println("failed to load class " + e);
+                     //       System.out.println(className.length());
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+        } catch (IOException e) {
+                // TODO Auto-generated catch block
+               // e.printStackTrace();
+            }
+            //if it didnt work, we are not from  a .jar and can use the old method. We will know by checking if allInteracts is empty
+            if(allInteracts.size() > 0){
+                System.out.println("Loaded types from .jar");
+                return;
+            }
+            System.out.println(".jar load failed retrying with different method, checking for java files");
         File folder = new File(".");
         initDirc(folder, "");
         for (Interactable i : allInteracts) {
@@ -269,7 +311,7 @@ public class Main {
                     s.newInstance();
 
                 } catch (Exception ignored) {
-                    System.out.println(ignored);
+                    //System.out.println(ignored);
                 }
             }
             else if (listOfFile.isDirectory()) {
